185e52ef06c45f5881af6a1250ca3b101e079473
Rabin <rabin.awale@krafters.dev>
2025-04-07 15:31:42 +0545

    Implement AdminController and update routes for home navigation

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function index() {
+        $user = Auth()->user();
+        if($user){
+            return view('admin.index');
+        }
+        else{
+            return redirect()->back();
+        }
+    }
+}
